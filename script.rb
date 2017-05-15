ec432d90305c1fb81d3e0fe73a5a0b5b3175f18d
Benoit Molenda <benoit@proxygear.com>
2017-05-15 16:26:40 +0200

    Fix config loading

--- script.rb
+++ script.rb
@@ -3,7 +3,8 @@
 require_relative './src/time_lapse.rb'
 
 begin
-  res = TimeLapse.from_config_file('./config.yml').call
+  config_path = File.join File.dirname(__FILE__), 'config.yml'
+  res = TimeLapse.from_config_file(config_path).call
   File.open('./script.log', 'a') {|f| f.write "#{Time.now}: OK - #{res}\n" }
   puts res
 rescue Exception => e
