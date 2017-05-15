4605702bf2ccaa1bcc5e2a15358ec3f7e070c28f
Benoit Molenda <benoit@proxygear.com>
2017-05-15 12:56:45 +0200

    Fix TimeLapse class usage

--- script.rb
+++ script.rb
@@ -2,7 +2,7 @@
 
 require './time_lapse'
 
-puts TimeLapse.from_yaml('./config.yml').call
+puts TimeLapse.load_yaml('./config.yml').call
 git@github.com:proxygear/tiny_ruby_time_lapse.git
 
 https://github.com/proxygear/tiny_ruby_time_lapse.git
