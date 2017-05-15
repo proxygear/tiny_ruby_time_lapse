e088d311d64dd288f3f3987885037d50e67ea78d
Benoit Molenda <benoit@proxygear.com>
2017-05-15 12:53:15 +0200

    Fix shebang

--- script.rb
+++ script.rb
@@ -1,5 +1,8 @@
-#!/usr/bin/env/ruby
+#!/usr/bin/env ruby
 
 require './time_lapse'
 
 puts TimeLapse.from_yaml('./config.yml').call
+git@github.com:proxygear/tiny_ruby_time_lapse.git
+
+https://github.com/proxygear/tiny_ruby_time_lapse.git
