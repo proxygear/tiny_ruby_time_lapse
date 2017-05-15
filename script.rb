5c6ab68ff3faa155c217dfe5fee19acfa7d415f7
Benoit Molenda <benoit@proxygear.com>
2017-05-15 14:45:18 +0200

    add a log system :)

--- script.rb
+++ script.rb
@@ -2,4 +2,11 @@
 
 require './src/time_lapse'
 
-puts TimeLapse.from_config_file('./config.yml').call
+begin
+  res = TimeLapse.from_config_file('./config.yml').call
+  File.open('./script.log', 'a') {|f| f.write "#{Time.now}: OK - #{res}\n" }
+  puts res
+rescue Exception => e
+  File.open('./script.log', 'a') {|f| f.write "#{Time.now}: #{e.message}\n" }
+  raise e
+end
